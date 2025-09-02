c3574bf8e645843830cab5e99def70c624669595
Dazz <2798605781@qq.com>
2025-09-02 12:35:27 +0800

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        float f =123f;
+        double d = 123d;
+        System.out.println("f="+f);
+        System.out.println("d="+d);
+        System.out.println("Hello world!");
+        
+    }
+}
