5a52e65c93c6a5586c214b9b76e492ed5b8431a1
Dazz <2798605781@qq.com>
2025-09-04 14:25:59 +0800

    feat:update exception

--- src/main/java/Exception/Main.java
+++ src/main/java/Exception/Main.java
@@ -1,13 +1,34 @@
 package Exception;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+//        try {
+//            int one =input.nextInt();
+//            int two = input.nextInt();
+//            System.out.println("两数商为"+(one/two));
+//        }catch (Exception e){
+//            e.printStackTrace();//打印错误信息
+//            System.out.println("程序出错了");
+//        }finally {
+//            System.out.println("运算结束");
+//        }
+        try {
+            int one =input.nextInt();
+            int two = input.nextInt();
+            System.out.println("两数商为"+(one/two));
+        }catch (ArithmeticException e){
+            e.printStackTrace();//打印错误信息
+            System.out.println("除数不为0");
+        }catch (InputMismatchException e){
+          System.out.println("请输入整数");
+        } finally {
+            System.out.println("运算结束");
+        }
 
-        float f =123f;
-        double d = 123d;
-        System.out.println("f="+f);
-        System.out.println("d="+d);
-        System.out.println("Hello world!");
 
     }
 }
